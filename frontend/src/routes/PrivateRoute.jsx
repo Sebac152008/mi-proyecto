94653b4bad7923e9afc10e8c61b1ce6beb852beb
Sebac152008 <sebac152008@gmail.com>
2025-07-15 22:32:00 -0400

    octavo commit

--- frontend/src/routes/PrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-// src/routes/PrivateRoute.jsx
-import { Navigate } from "react-router-dom";
-
-const PrivateRoute = ({ children }) => {
-  const isAuthenticated = localStorage.getItem("isAuthenticated") === "true";
-  return isAuthenticated ? children : <Navigate to="/login" />;
-};
-
-export default PrivateRoute;
